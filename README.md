a1459b300b08eff541d5aacb8a72c981eb6bc681
tom <tom@x2k.co.uk>
2012-07-13 18:07:06 +0100

    jQuery plugin version

---
+++ README.md
@@ -0,0 +1,16 @@
+= jInsects =
+
+== Description ==
+
+jInsects is a jQuery plugin which generates bugs under the specified element
+which then run off screen.
+
+This concept for this idea came from Lee and the implementation by Tom Oram.
+
+
+== Usage ==
+
+
+== Links ==
+
+http://devblog.x2k.co.uk/
--- js/jquery.insects.js
+++ js/jquery.insects.js
@@ -1,124 +1,120 @@
 (function ($) {
-	
-}(jQuery));
+	function Insect($layer, $window) {
+		function Vector2D(x, y) {
+			this.x = x;
+			this.y = y;
 
-function I($layer, $window) {
-	function V2D(x, y) {
-		this.x = x;
-		this.y = y;
+			this.add = function (vec2d) {
+				this.x += vec2d.x;
+				this.y += vec2d.y;
+			};
 
-		this.add = function (vec2d) {
-			this.x += vec2d.x;
-			this.y += vec2d.y;
+			this.getRotated = function (r) {
+				var x = (this.x * Math.cos(r) + this.y * -Math.sin(r)),
+					y = (this.x * Math.sin(r) + this.y * Math.cos(r));
+
+				return new Vector2D(x, y);
+			};
 		}
 
-		this.getRotated = function (r) {
- 			var x = (this.x * Math.cos(r) + this.y *-Math.sin(r)),
-				y = (this.x * Math.sin(r) + this.y * Math.cos(r));
 
-			return new V2D(x, y);
-		}
-	}
+		this.create = function () {
+			this.$element = $('<div class="jq-insect"></div>').css({
+				position: 'absolute',
+				top: this.position.y + 'px',
+				left: this.position.x + 'px',
+				'z-index': $layer.css('z-index') - 1
+			});
 
-	this.randomRotate = function () {
-		if (!this.isAlive()) {
-			return;
-		}
+			$('body').append(this.$element);
 
-		this.rotation += Math.random() * 1.5 - 0.75;
-		this.rotateElement();
+			this.randomRotate();
+			this.update();
+		};
 
-		var insect = this;
-		setTimeout(function () {
-			insect.randomRotate();
-		}, Math.random() * 1000);
-	}
+		this.rotateElement = function () {
+			var rotate = 'rotate(' + this.rotation + 'rad)';
 
-	this.create = function() {
-		this.$element = $('<div class="jq-insect"></div>').css({
-			position: 'absolute',
-			top: this.position.y + 'px',
-			left: this.position.x + 'px'
-		});
+			this.$element.css({
+				'-webkit-transform': rotate,
+				'-moz-transform': rotate,
+				'-ms-transform': rotate
+			});
+		};
 
-		$('body').append(this.$element);
-		
-		this.randomRotate();
-		this.update();
-	}
-
-	this.kill = function () {
-		this.alive = false;
-		this.$element.remove();
-	}
-
-	this.isOffscreen = function (width, height) {
-		if (this.position.x < -50 || this.position.y < -50
-		|| this.position.x > $window.width() || this.position.y > $window.height()) {
-			this.kill();
-		}
-	}
-
-	this.update = function(delta) {
-		if (!this.alive) {
-			return false;
-		}
-
-		if (this.isOffscreen(window.width, window.height)) {
-			this.kill();
-		}
-
-		this.position.add(this.direction.getRotated(this.rotation));
-
-		this.$element.css({
-			top: this.position.y + 'px',
-			left: this.position.x + 'px'
-		});
-
-		var insect = this;
-		setTimeout(function () {
-			insect.update();
-		}, 50);
-	}
-
-	this.isAlive = function() {
-		return this.alive;
-	}
-
-	this.rotateElement = function() {
-		var rotate = 'rotate(' + this.rotation + 'rad)';
-
-		this.$element.css({
-			'-webkit-transform': rotate,
-			'-moz-transform': rotate,
-			'-ms-transform': rotate
-		});
-	}
-
-	var offset = $layer.offset(),
-		x = offset.left + $layer.width() / 2,
-		y = offset.top + $layer.height() / 2;
-
-	this.position = new V2D(x, y);
-	this.direction = new V2D(0, 10);
-	this.rotation = (Math.random() * 2 * Math.PI);
-	this.alive = true;
-	this.create();
-}
-
-$(document).ready(function () {
-	$('#activate').hover(
-		function () {
-			$('#popup').show();
-		},
-		function () {
-			var count = Math.floor(Math.random() * 3),
-				insects = [];
-			for (i = 0; i <= count; i++) {
-				insects.push(new I($('#popup'), $(window)));
+		this.randomRotate = function () {
+			if (!this.isAlive()) {
+				return;
 			}
 
-			$('#popup').hide();
-		}
-	);
-});
+			this.rotation += Math.random() * 1.5 - 0.75;
+			this.rotateElement();
+
+			var insect = this;
+			setTimeout(function () {
+				insect.randomRotate();
+			}, Math.random() * 1000);
+		};
+
+		this.isAlive = function () {
+			return this.alive;
+		};
+
+		this.kill = function () {
+			this.alive = false;
+			this.$element.remove();
+		};
+
+		this.isOffscreen = function () {
+			if (this.position.x < -50
+					|| this.position.y < -50
+					|| this.position.x > $window.width()
+					|| this.position.y > $window.height()) {
+				this.kill();
+			}
+		};
+
+		this.update = function (delta) {
+			if (!this.alive) {
+				return false;
+			}
+
+			if (this.isOffscreen(window.width, window.height)) {
+				this.kill();
+			}
+
+			this.position.add(this.direction.getRotated(this.rotation));
+
+			this.$element.css({
+				top: this.position.y + 'px',
+				left: this.position.x + 'px'
+			});
+
+			var insect = this;
+			setTimeout(function () {
+				insect.update();
+			}, 50);
+		};
+
+		var offset = $layer.offset(),
+			x = offset.left + $layer.width() / 2,
+			y = offset.top + $layer.height() / 2;
+
+		this.position = new Vector2D(x, y);
+		this.direction = new Vector2D(0, 10);
+		this.rotation = (Math.random() * 2 * Math.PI);
+		this.alive = true;
+		this.create();
+	}
+
+	$.fn.insectify = function () {
+		return this.each(function (index, element) {
+			var count = Math.floor(Math.random() * 3),
+				insects = [],
+				i;
+			for (i = 0; i <= count; i += 1) {
+				insects.push(new Insect($(element), $(window)));
+			}
+		});
+	};
+}(jQuery));
